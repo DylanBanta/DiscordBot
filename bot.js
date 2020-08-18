f473fa798eda9d27b5ce06ed75ef4128cf358bd8
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-18 18:18:32 -0400

    .

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
 function getRandomInt(max) {
   return (Math.floor(Math.random() * Math.floor(max)) + 1);
