69d0982b4b9f3877bba5177d29ece70f83d9bffc
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-18 19:39:15 -0400

    .

--- bot.js
+++ bot.js
@@ -1,26 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-
-function getRandomInt(max) {
-  return (Math.floor(Math.random() * Math.floor(max)) + 1);
-}
-
-function roll(message, user, count, die) {
-  message.reply(user + " is rolling " + count + "d" + die);
-  var total = 0;
-  var diceArr = new Array(count);
-
-  for (var i = 0; i < count; i++) {
-    diceArr[i] = getRandomInt(die);
-  }
-
-  for (var j = 0; j < diceArr.length; j++) {
-    total += diceArr[j];
-  }
-
-  message.reply(user + " rolled " + diceArr.toString() + " for a total of " + total);
-}
+const Dice = require("tabletop-dice");
 
 client.on("ready", () => {
   console.log("D&D Bot Has Rolled for Initiative"); //D&D bot is on
@@ -29,18 +10,34 @@ client.on("ready", () => {
 client.on("message", message => {
   var user = message.member.displayName;
   var prefix = config.prefix;
+  var input = message.content;
+  var dBool = false;
+  var dString;
 
-  switch (message.content) {
-    case prefix + "d4":
-    case prefix + "1d4":
-      roll(message, user, 1, 4);
-      break;
-    case "d6":
-    case "1d6":
-      message.reply("Rolling D6");
-      roll = getRandomInt(6);
-      console.log("roll | " + roll);
+  console.log("input | " + input);
+
+  if (input.startsWith(prefix)) {
+    //Splits command after prefix. (input "!1" becomes "1");
+    input = input.split(prefix)[1];
+    dBool = input.includes("d");
+    if (dBool) {
+      dString = input.split("d");
+      for (var i = 0; i < dString.length; i++) {
+        console.log("dString[" + i + "] | " + dString[i]);
+      }
+    }
   }
+
+  // switch (message.content) {
+  //   case prefix + "d4":
+  //   case prefix + "1d4":
+  //     roll(message, user, 1, 4);
+  //     break;
+  //   case prefix + "d6":
+  //   case prefix + "1d6":
+  //     message.reply("Rolling D6");
+  //     roll = getRandomInt(6);
+  // }
 });
 
 //Logs in
