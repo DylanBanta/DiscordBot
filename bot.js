070395aadef0752deaf3f9d923fe9ecedcb77528
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-18 03:04:52 -0400

    Basic Setup for Heroku
    https://medium.com/@mason.spr/hosting-a-discord-js-bot-for-free-using-heroku-564c3da2d23f

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on('ready', () => {
+  console.log('D&D Bot Has Rolled for Initiative'); //D&D bot is on
+})
+
+client.on('message', message => {
+  var prefix = '!';
+  if (message.content === prefix + 'd20') {
+    message.reply('Rolling D20');
+  }
+});
+
+client.login(client.token);
