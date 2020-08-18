fef47229d6bfd0549347ad773158f8de11874c2a
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-18 18:37:50 -0400

    .

--- bot.js
+++ bot.js
@@ -6,29 +6,38 @@ function getRandomInt(max) {
   return (Math.floor(Math.random() * Math.floor(max)) + 1);
 }
 
-function roll(count, die) {
-  message.reply('Rolling ' + count + ' d' + die);
-  roll = getRandomInt(4);
-  console.log("roll | " + roll);
-}
+function roll(user, count, die) {
+  message.reply(user + ' is rolling ' + count + 'd' + die);
+  var total = 0;
+  var diceArr = new Array(count);
 
-client.on('ready', () => {
-  console.log('D&D Bot Has Rolled for Initiative'); //D&D bot is on
-})
-
-client.on('message', message => {
-  var nick = message.member.displayName;
-  console.log("nick | " + nick);
-  switch (message.content) {
-    case "!d4":
-      //roll(1, 4);
-      break;
-    case "!d6":
-      message.reply('Rolling D6');
-      roll = getRandomInt(6);
-      console.log("roll | " + roll);
+  for (var i = 0; i < count; i++) {
+    diceArr[i] = getRandomInt(die);
   }
-});
 
-//Logs in
-client.login(process.env.BOT_TOKEN);
+  for (var j = 0; j < diceArr.length; j++) {
+    total += diceArr[j];
+  }
+
+  message.reply(user + ' rolled ' + diceArr.toString() + ' for a total of ' + total;
+  }
+
+  client.on('ready', () => {
+    console.log('D&D Bot Has Rolled for Initiative'); //D&D bot is on
+  })
+
+  client.on('message', message => {
+    var user = message.member.displayName;
+    switch (message.content) {
+      case "!d4":
+        roll(user, 1, 4);
+        break;
+      case "!d6":
+        message.reply('Rolling D6');
+        roll = getRandomInt(6);
+        console.log("roll | " + roll);
+    }
+  });
+
+  //Logs in
+  client.login(process.env.BOT_TOKEN);
