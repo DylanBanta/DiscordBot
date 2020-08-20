aebbe12bcd0dc47df838e042628e8d78008d4518
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-20 16:29:01 -0400

    .

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const Dice = require("./dice.js");
+let Dice = require("tabletop-dice");
 
 client.on("ready", () => {
   console.log("D&D Bot Has Rolled for Initiative"); //D&D bot is on
