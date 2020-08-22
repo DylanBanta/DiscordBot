3ce76a948d415cfbebf02f21b39d04d9d3152d93
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-22 14:23:01 -0400

    .

--- dm/games/treasure_hunt/treasure_hunt.js
+++ dm/games/treasure_hunt/treasure_hunt.js
@@ -4,6 +4,10 @@ class TrHunt {
 
   test() {
     console.log("TrHunt");
+
+
+    var goldChest = new Treasure("Coins", 600, "A treasure chest filled with gold coins");
+    console.log(goldChest);
     /*
     //var Rooms = require(".rooms.js");
     var Treasure = require("./treasure.js");
