7bb7c4b7c8c7a37a55689d7814aa1a2ff2fe0d64
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-22 14:47:48 -0400

    .

--- dm/games/treasure_hunt/treasure_hunt.js
+++ dm/games/treasure_hunt/treasure_hunt.js
@@ -7,20 +7,20 @@ class Th { //Threasure Hunt Class
   test() {
     console.log("TrHunt");
     var arr = Treasure.array();
-    var json = JSON.stringify(arr));
-  fs.writeFile("./treasure.json", arr);
-  console.log("TrHunt Exit");
+    var json = JSON.stringify(arr);
+    fs.writeFile("./treasure.json", arr);
+    console.log("TrHunt Exit");
 
-  /*
-  //var Rooms = require(".rooms.js");
-  var Treasure = require("./treasure.js");
+    /*
+    //var Rooms = require(".rooms.js");
+    var Treasure = require("./treasure.js");
 
-  var treasureArr = Treasure.array();
-  for (var i = 0; i < treasureArr.length; i++) {
-    console.log("treasureArr[" + i + "] | " + treasureArr[i]);
+    var treasureArr = Treasure.array();
+    for (var i = 0; i < treasureArr.length; i++) {
+      console.log("treasureArr[" + i + "] | " + treasureArr[i]);
+    }
+    */
   }
-  */
-}
 }
 
 module.exports = new Th();
