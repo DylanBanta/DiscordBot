daa6611d519ed77252ebf85dad2f3d7db25a8aa4
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-21 22:37:45 -0400

    .

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on("message", message => {
     var input = input.split(prefix)[1];
 
     if (input == "Debug") {
-      console.log(Dm);
+      Dm.test("Test");
     }
 
     if (input == "Save") {
