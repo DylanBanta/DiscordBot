a0133bdc49c919c65a95ebf0fb0cc83646aaa91e
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-08-22 00:41:21 -0400

    .

---
+++ node_modules/discord-module/discord-module.js
@@ -0,0 +1,6 @@
+console.log("DM");
+
+// exports.debug = require("./utils/debug.js");
+// exports.read_write_files = require("./utils/read_write_files.js");
+// exports.rooms = require("./games/treasure_hunt/rooms.js");
+// exports.treasure = require("./games/treasure_hunt/treasure.js");
