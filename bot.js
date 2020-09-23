e6762269eab844918eebdaa0a8a81a532598fe6d
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-09-23 15:38:33 -0400

    Refactoring

--- bot.js
+++ bot.js
@@ -15,35 +15,7 @@ client.on("ready", () => {
 
 //When the client detects a message
 client.on("message", message => {
-  var user = message.member.displayName; //User who posted the message
-  var prefix = config.prefix; //checks config file for command input prefix
-  var input = message.content; //Message content
-  var dOutput; //Initialize the eventual output variable
-
-  if (input.startsWith(prefix)) { //Checks if the input begins witht he prefix, if so, seperates out prefix from the Command
-    //Splits command after prefix. (input "!1" becomes "1");
-    var input = input.split(prefix)[1];
-
-    if (input == "Debug") {
-      Dm.debug.log("Test?");
-      Dm.th.test();
-    }
-
-    if (input == "Save") {
-      //Dm.writeFile("./Test.txt", "Test Data");
-      //message.reply("Saved Test Data.");
-    }
-
-
-    var dBool = false;
-    //Check for Dice Roll Command
-    dBool = input.includes("d");
-    if (dBool) { //TODO make sure to only call this when actually rolling a die
-      dOutput = Dice.diceCommand(input);
-      message.reply(dOutput);
-    }
-  }
-
+  Dm.messageHandler.messageHandler(message);
 });
 
 //Logs in
