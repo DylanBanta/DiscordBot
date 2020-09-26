93ff6eea1de192178d636353d7e221d45354b225
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-09-26 01:49:19 -0400

    .

--- mods/utils/npc/npc.js
+++ mods/utils/npc/npc.js
@@ -25,13 +25,13 @@ class Npc {
     var race = Race.defaultRace();
     console.log("race", race);
     console.log("gender", race.gender);
-    // var data = JSON.stringify(race);
-    // var fileLocation = "/app/mods/npc/";
-    // var fileName = "defaultRace";
-    // var ext = ".json";
-    //
-    // Util.saveFile(fileLocation, fileName, ext, data);
-    // console.log("saveRace() end");
+    var data = JSON.stringify(race);
+    var fileLocation = "/app/mods/npc/";
+    var fileName = "defaultRace";
+    var ext = ".json";
+
+    Util.saveFile(fileLocation, fileName, ext, data);
+    console.log("saveRace() end");
   }
 }
 
